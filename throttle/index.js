5b435ac8b2a2d3592b4d686deea61c50d8967a8d
guangjun <547652008@qq.com>
2019-10-06 01:01:02 +0800

    fix: throttle

--- throttle/index.js
+++ throttle/index.js
@@ -13,21 +13,28 @@ function debounce (fn ,delay) {
  * 节流: 连续滚动，n秒内会执行一次
  */
 function throttle (fn ,delay) {
-  let timeStamp = +new Date();
+  let timer;
+  let timeStamp;
 
   return (...args) => {
-
     const now = +new Date();
+    if (!timeStamp) timeStamp = now;
+
     if (now - timeStamp > delay) {
       timeStamp = now;
       fn(...args);
+    } else {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        fn(...args);
+        timeStamp = 0; // 防止很久没滚动，突然滚动了，就直接执行了，配合‘if (!timeStamp) timeStamp = now’
+      }, delay);
     }
-
   }
 }
 
 const fn = (a) => {
   console.log(a);
 }
-window.onscroll = debounce(fn.bind(this, 'debounce'), 1000)
-// window.onscroll = throttle(fn.bind(this, 'throttle'), 1000)
+// window.onscroll = debounce(fn.bind(this, 'debounce'), 1000)
+window.onscroll = throttle(fn.bind(this, 'throttle'), 1000)
