14e5435ab6e4164bccf9395e97c3eb2baf17de18
guangjun <547652008@qq.com>
2019-10-19 20:09:17 +0800

    fix: debug skill

--- debug-skill/Proxy.js
+++ debug-skill/Proxy.js
@@ -2,9 +2,14 @@ const obj = {
     a: 1
 };
 
+// 不知道代码哪里修改了值的情况下可以使用Proxy
 Proxy(obj, {
     set(target, key, value, receiver) {
         debugger;
         return Reflect.set(target, key, value, receiver);
     }
+})
+
+setTimeout(() => {
+    obj.a = 2;
 })
