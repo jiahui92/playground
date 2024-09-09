4bc2568342abed2b72ac57fa32c5924f661af245
jiahui92 <547652008@qq.com>
2024-09-10 00:06:43 +0800

    update: debug and script

--- nestjs/script/generate-entities.ts
+++ nestjs/script/generate-entities.ts
@@ -1,20 +1,17 @@
 import { execSync } from 'child_process';
-import * as dotenv from 'dotenv';
 
-// 设置.env配置
-dotenv.config();
-
-const entities_path = './src/entities';
+const entities_path = './src/graphql';
 
 // 删除旧文件 rm -rf
-const del = `rimraf entities_path`;
+const del = `rimraf ${entities_path}`;
 
 // 生成entities
 // --noConfig true 不自动生成ormconfig,tsconfig
 // --cp camel 表示将数据库中的字段比如create_at转换为createAt
-const generate = `npx typeorm-model-generator -h ${process.env.DB_HOST} -d ${process.env.DB_DATABASE} -u ${process.env.DB_USER} -x ${process.env.DB_PASSWORD} -e mysql --output ${entities_path} --noConfig true --cp camel`;
+const generate = `npx pal generate -c ./prisma/pal.js`;
 
 // prettier格式化生成好的代码
 const prettier = `npx prettier --write ${entities_path}`;
 
-execSync(`${del} && ${generate} && ${prettier}`, { encoding: 'utf-8' });
+execSync(`${del} && ${generate}`);
+execSync(`${prettier}`, { encoding: 'utf-8' });
