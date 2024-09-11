0a25be899c66b36244cf0e1ab03543d3d8247195
jiahui92 <547652008@qq.com>
2024-09-11 16:43:58 +0800

    add: prisma example

--- nestjs/src/app.controller.ts
+++ nestjs/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { NexusGenObjects } from './generated/typings';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('getManyCity')
+  async getManyCity(): Promise<NexusGenObjects['city'][]> {
+    return await this.appService.getManyCity();
   }
 }
--- nestjs/src/app.service.ts
+++ nestjs/src/app.service.ts
@@ -1,8 +1,23 @@
+import { PrismaService } from './prisma.service';
 import { Injectable } from '@nestjs/common';
+import { NexusGenObjects } from './generated/typings';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
+
   getHello(): string {
     return 'Hello World! jiahui';
+  }
+
+  async getManyCity(): Promise<NexusGenObjects['city'][]> {
+    const cities = await this.prisma.city.findMany({
+      // take: 10,
+      // select: { Name: true },
+      where: {
+        CountryCode: { equals: 'USA' },
+      },
+    });
+    return cities;
   }
 }
